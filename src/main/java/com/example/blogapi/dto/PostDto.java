a2a778c2467ac9cd0bb148a9382c1a819a300720
DawidRozewski <dawid.rrozewski@gmail.com>
2022-02-16 19:00:34 +0100

    add `PostDto`

---
+++ src/main/java/com/example/blogapi/dto/PostDto.java
@@ -0,0 +1,10 @@
+package com.example.blogapi.dto;
+import lombok.Data;
+
+@Data
+public class PostDto {
+    private long id;
+    private String title;
+    private String description;
+    private String content;
+}
