402766fd99207049707e81eba96fa99e8d9c8fd8
DawidRozewski <dawid.rrozewski@gmail.com>
2022-02-16 18:51:46 +0100

    add `post` resource

---
+++ src/main/java/com/example/blogapi/model/Post.java
@@ -0,0 +1,27 @@
+package com.example.blogapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String title;
+
+    @Column(unique = true, nullable = false)
+    private String description;
+
+    @Column(unique = true, nullable = false)
+    private String content;
+}
