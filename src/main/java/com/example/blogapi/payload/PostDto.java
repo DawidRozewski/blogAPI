1dead5f36197016f727526f5eff53a69e658440b
DawidRozewski <dawid.rrozewski@gmail.com>
2022-02-18 19:20:33 +0100

    validation

--- src/main/java/com/example/blogapi/payload/PostDto.java
+++ src/main/java/com/example/blogapi/payload/PostDto.java
@@ -2,6 +2,8 @@ package com.example.blogapi.payload;
 
 import lombok.*;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.Set;
 
 @Getter
@@ -10,8 +12,16 @@ import java.util.Set;
 @AllArgsConstructor
 public class PostDto {
     private long id;
+
+    @NotEmpty
+    @Size(min = 2, message = "Post title should have at least 2 characters")
     private String title;
+
+    @NotEmpty
+    @Size(min = 10, message = "Post description should have at least 10 characters")
     private String description;
+
+    @NotEmpty
     private String content;
     private Set<CommentDto> comments;
 }
