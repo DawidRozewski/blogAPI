fb5e75d9cff5414a4fd282f1fee73d8ddba83eda
DawidRozewski <dawid.rrozewski@gmail.com>
2022-02-21 18:44:21 +0100

    Versioning through query parameters

--- src/main/java/com/example/blogapi/controller/PostController.java
+++ src/main/java/com/example/blogapi/controller/PostController.java
@@ -33,12 +33,12 @@ public class PostController {
         return postService.getAllPost(pageNo, pageSize, sortBy, sortDir);
     }
 
-    @GetMapping("/api/v1/posts/{id}")
+    @GetMapping(value = "/api/posts/{id}", params = "version=1")
     public ResponseEntity<PostDto> getPostByIdV1(@PathVariable long id) {
         return ResponseEntity.ok(postService.getPostById(id));
     }
 
-    @GetMapping("/api/v2/posts/{id}")
+    @GetMapping(value = "/api/posts/{id}", params = "version=2")
     public ResponseEntity<PostDtoV2> getPostByIdV2(@PathVariable long id) {
         PostDtoV2 postDtoV2 = postService.clonePostDto(id);
         return ResponseEntity.ok(postDtoV2);
