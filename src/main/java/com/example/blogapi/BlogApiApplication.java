629cb53fea5884a88d69181c93f3cef83bc9b87c
DawidRozewski <dawid.rrozewski@gmail.com>
2022-02-22 11:18:03 +0100

    Insert Metadata in Table

--- src/main/java/com/example/blogapi/BlogApiApplication.java
+++ src/main/java/com/example/blogapi/BlogApiApplication.java
@@ -1,14 +1,23 @@
 package com.example.blogapi;
 
+import com.example.blogapi.model.Role;
+import com.example.blogapi.repository.RoleRepository;
+import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+
+@AllArgsConstructor
 @SpringBootApplication
 @EnableWebMvc
-public class BlogApiApplication {
+public class BlogApiApplication implements CommandLineRunner {
+
+    private final RoleRepository roleRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(BlogApiApplication.class, args);
@@ -17,5 +26,17 @@ public class BlogApiApplication {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        setRoleInDB("ROLE_ADMIN");
+        setRoleInDB("ROLE_USER");
+    }
+
+    private void setRoleInDB(String role_user) {
+        Role userRole = new Role();
+        userRole.setName(role_user);
+        roleRepository.save(userRole);
     }
 }
